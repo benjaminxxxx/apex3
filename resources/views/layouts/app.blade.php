deb83012c8176583849a2ec7be07f47b0baa3d06
Benjaimin <benjamin_unitek@hotmail.com>
2024-06-23 09:25:44 -0500

    primero cambio

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    <link href="{{asset('css/fontello.css')}}" rel="stylesheet" /> 
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet" /> 
+    @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+    <x-banner />
+
+    <div class="min-h-screen bg-gray-100" x-data="{ isOpenMenu: false }" x-bind:class="{ 'is-active-menu': isOpenMenu }">
+        @livewire('navigation-menu')
+
+        <aside id="logo-sidebar" x-bind:class="{ 'active-menu-width': isOpenMenu }"
+            class="fixed top-0 left-0 z-40 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 menu-width"
+            aria-label="Sidebar">
+            
+            <div class="h-full px-3 pb-4 overflow-y-auto dark:bg-gray-800 md:p-7" style="background:#FCFCFC">
+                <ul class="space-y-2 font-medium" x-bind:class="{ 'active-menu': isOpenMenu, 'inactive-menu': !isOpenMenu }">
+              
+                   <x-nav-link href="/fff" icon="icon-telegram-2" menu="Actividad" />
+                   <x-nav-link href="/fff" icon="icon-user-3" menu="Miembros" />
+                   <x-nav-link href="/fff" icon="icon-signal-2" menu="Grupos" />
+                   <x-nav-link href="/fff" icon="icon-copy" menu="Eventos" />
+                   <x-nav-link href="/fff" icon="icon-telegram-2" menu="Documentos" />
+                   <x-nav-link href="/fff" icon="icon-feather-2" menu="Noticias" />
+                   <x-nav-link href="/fff" icon="icon-email-3" menu="Contacto" />
+                   
+                </ul>
+            </div>
+        </aside>
+
+        <div class="p-4 main-container h-screen mt-20" style="box-shadow:inset 0 0 15px 5px #e4e4e4">
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/navigation-menu.blade.php
@@ -0,0 +1,80 @@
+<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <div class="">
+        <div class="flex items-center justify-between">
+
+            <div class="flex items-center justify-between rtl:justify-end top-aside h-20">
+
+                <a href="https://flowbite.com" class="flex ms-2 md:me-24 link-logo p-2 md:p-3">
+                    <img src="{{ asset('/images/logo.png') }}" alt="logo" />
+                </a>
+                <div class="bars-container md:w-[110px] text-center">
+                    <button x-on:click="isOpenMenu = !isOpenMenu" type="button"
+                        class="inline-flex items-center p-2 text-sm text-gray-500 hover:text-indigo-700 ">
+                        <span class="sr-only">Open sidebar</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd" fill-rule="evenodd"
+                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
+            <div class="flex items-center p-2 md:p-3">
+                <div class="flex items-center ms-3">
+                    <div class="ms-3 relative">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button
+                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-8 w-8 rounded-full object-cover"
+                                            src="{{ Auth::user()->profile_photo_url }}"
+                                            alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
+
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
+
+                                <x-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+                                
+                                <div class="border-t border-gray-200"></div>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
